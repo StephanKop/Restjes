c3cc8f3e4f69cba2a8fb76bf205fa4559a9724a9
Stephan Kop <stephan.kop@strix.net>
2026-04-15 13:59:05 +0200

    added analytics

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -54,6 +54,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
           })(window, document, "clarity", "script", "wbotmzule1");`}
         </Script>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-2N6YN3XBRW" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-2N6YN3XBRW');`}
+        </Script>
         <Suspense>
           <NavigationProgress />
         </Suspense>
